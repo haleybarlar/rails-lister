86e7667e562c0adcec72e8b2be179a1878ebabf4
haleybarlar <hbarlar1@gmail.com>
2018-12-01 19:32:47 -0500

    auth complete

--- app/serializer/list_serializer.rb
+++ app/serializer/list_serializer.rb
@@ -1,4 +1,4 @@
 class ListSerializer < ActiveModel::Serializer
-  attributes :id, :kind, :created_at, :done, :time_completed, :created_at
+  attributes :id, :kind, :created_at, :done, :time_completed, :created_at, :user_id
   has_many :tasks
 end
